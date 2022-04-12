dc6e740727a24d49cf7292ec86a33ff7ae8b43b2
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2022-04-13 01:06:45 +0200

    Create login form frontend

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+const matModules = [
+	MatFormFieldModule,
+	MatInputModule,
+	MatIconModule,
+	MatButtonModule
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		matModules,
+		AppRoutingModule
+	],
+	exports: [
+		matModules
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
