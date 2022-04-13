b415762a79f5dc53c94b40aa142b37e7a30436d9
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2022-04-13 20:50:07 +0200

    Add registeration page frontend

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const matModules = [
 	MatFormFieldModule,
@@ -22,6 +23,7 @@ const matModules = [
 	declarations: [
 		AppComponent,
 		LoginComponent,
+  RegisterComponent,
 	],
 	imports: [
 		BrowserModule,
