9ae2e3505f4b82fd2e5f181635a1301a0b8a41ab
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2022-04-17 21:45:09 +0200

    Validate user info

--- Users/Models/User.cs
+++ Users/Models/User.cs
@@ -12,10 +12,8 @@ namespace Users.Models
 		public string? Name { get; set; }
 
 		[Required]
-		[EmailAddress]
 		public string? Email { get; set; }
 
-		[Phone]
 		public string? Phone { get; set; }
 
 		public string? Address { get; set; }
